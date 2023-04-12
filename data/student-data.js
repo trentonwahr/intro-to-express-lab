1bc2135aa8902b19bb86811a7037184e3b5d9ab5
trentonwahr <trentwahr@gmail.com>
2023-04-12 16:54:59 -0400

    finish lab

---
+++ data/student-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {name: 'Ricky', passing: false, _id: 125266},
+  {name: 'Bobby', passing: true, _id: 125245},
+  {name: 'Tommy', passing: false, _id: 125278},
+]
+
+export {
+  students
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // import modules
 
 import express from 'express'
+import { students } from './data/student-data.js'
 
 // create Express app
 
@@ -8,7 +9,7 @@ const app = express()
 
 // configure the app (app.set)
 
-
+app.set('view engine', 'ejs')
 
 // mount Middleware (app.use)
 
@@ -16,9 +17,19 @@ const app = express()
 
 // mount routes
 
+app.get('/', function(req,res) {
+  res.redirect('/home')
+})
+
 app.get('/home', function(req,res) {
   res.render('home')
 })
+
+app.get('/students', function(req, res) {
+  res.render('students/index', {
+    students: students
+  })
+}) 
 
 // tell the app to listen on port 3000
 
